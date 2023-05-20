0b05d20655df26b0a46cd01558da86e0959d8b35
Fabio <stella.vlassi01@gmail.com>
2023-05-20 20:41:32 +0300

    html -> hbs version 3 20/5

--- routes/router.mjs
+++ routes/router.mjs
@@ -1,26 +1,52 @@
 import express from 'express'
 const router = express.Router()
 
-// const homeController = await import(`controller/home.mjs`)
+// const homeController = await import(`../controller/home.mjs`);
 
 router.get('/', (req,res) => {
-    res.render('home')
-})
+    res.render('home',{
+        atHome: true
+    });
+});
+router.get('/home', (req,res) => {
+    res.render('home',{
+        atHome: true
+    });
+});
 router.get('/about', (req,res) => {
-    res.render('about')
-})
+    res.render('about',{
+        atAbout: true
+    });
+});
 router.get('/externalEvents', (req,res) => {
-    res.render('externalEvents')
-})
+    res.render('externalEvents',{
+        atExternalEvents: true
+    });
+});
 router.get('/internalEvents', (req,res) => {
-    res.render('internalEvents')
-})
+    res.render('internalEvents',{
+        atInternalEvents: true
+    });
+});
 router.get('/partners', (req,res) => {
-    // res.render('partners')
-    res.redirect('/partners');
-})
+    res.render('partners',{
+        atPartners: true
+    });
+});
 router.get('/contact', (req,res) => {
-    res.render('contact')
+    res.render('contact',{
+        atContact: true
+    });
+});
+router.get('/profile', (req,res) => {
+    res.render('profile',{
+        atProfile: true
+    });
+});
+router.get('/admin', (req,res) => {
+    res.render('admin',{
+        atAdmin: true
+    });
 })
 
 export default router;
