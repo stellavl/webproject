583c66b0c90e837b5d2459b42ddc341ea0b3d889
Fabio <stella.vlassi01@gmail.com>
2023-05-23 12:43:53 +0300

    minimal updates 23/5

--- app.mjs
+++ app.mjs
@@ -3,20 +3,23 @@ import { engine } from 'express-handlebars'
 import expSession from 'express-session'
 import bcrypt from "bcrypt";
 import createMemoryStore from 'memorystore';
+import dotenv from 'dotenv';
+
+if (process.env.NODE_ENV != 'production'){
+   dotenv.config();
+}
 
 const app = express()
 const PORT = process.env.PORT || '3000';
 
 //routes
 import routes from './routes/router.mjs'
-import { externalEvents } from './controller/externalEvents.mjs';
 app.use('/',routes);
 
 // Specifying that the "public" folder will contain the static files
 app.use(express.static('public'))
 
 // session
-// import 'dotenv/config';
 const MemoryStore = createMemoryStore(expSession);
 const sessionConf = {
     secret: process.env.secret || "έναμεγάλοτυχαίοαλφαριθμητικό",
@@ -37,24 +40,27 @@ app.use(express.urlencoded({extended: false}));
 
 app.post('/do-login', (req, res) => {
     const emailGiven = req.body.email;
-    const givenPassword = req.body.password;
+    const passwordGiven = req.body.password;
 
     const myPassword = '123'; 
 
     const saltRounds=10;
 
     const myPasswordHash = bcrypt.hash(myPassword, saltRounds);
-    const givenPasswordHash = bcrypt.hash(myPassword, saltRounds);
+    const passwordGivenHash = bcrypt.hash(passwordGiven, saltRounds);
 
-    bcrypt.compare(myPasswordHash, givenPasswordHash, (err, result) => {
+    bcrypt.compare(myPasswordHash, passwordGivenHash, (err, result) => {
     if (err) {
-        // Handle the error
         console.error(err);
     }
     if (result) {
         req.session.authenticatedEmail = emailGiven;
+        console.log("Authenticated");
+        console.log("Email:",emailGiven,"\nPassword:'",passwordGiven,"\nHash:",passwordGivenHash)
         return res.redirect('/profile');
     } else {
+        console.log("NOT authenticated");
+        console.log("Email:",emailGiven,"\nPassword:'",passwordGiven,"\nHash:",passwordGivenHash)
         return res.redirect('/home');
     }
     });
@@ -62,5 +68,5 @@ app.post('/do-login', (req, res) => {
 
 
 //starting server
-const server = app.listen(PORT, () => { console.log(`http://127.0.0.1:${PORT}`, externalEvents() )});
+const server = app.listen(PORT, () => { console.log(`http://127.0.0.1:${PORT}`)});
 
